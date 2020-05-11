f7a02c38a007d34a282049bf42809bf7059f7c48
S. Mert ÖZTÜRK <sametmertozturk1@gmail.com>
2020-05-12 00:18:33 +0300

    hava durumu entegrasyon v1

--- resources/views/parts/weather.blade.php
+++ resources/views/parts/weather.blade.php
@@ -1,47 +1,55 @@
+<?php
+$veriler = "http://api.openweathermap.org/data/2.5/forecast?id=740352&appid=4274f92d3edfaadfe581b1218493a4f4&lang=tr&units=metric";
+$homepage = file_get_contents($veriler);
+$coz = json_decode($homepage);
+setlocale(LC_TIME, "turkish"); //başka bir dil içinse burada belirteceksin.
+setlocale(LC_ALL,'turkish'); //başka bir dil içinse burada belirteceksin.
+?>
 <!-- START WEATHER -->
 <div class="weather-wrapper">
     <div class="row thm-margin">
         <div class="col-xs-3 col-sm-4 col-md-3 col-lg-3 weather-week thm-padding">
             <div class="list-group">
-                <a href="#" class="list-group-item active">
+                @for($i = 0; $i < 4; $i++)
+                    <?php
+                    $a = $i * 8;
+                    $gelen = $coz->list[$a];
+                    $tarih = gmdate("Y-m-d H:i:s", $gelen->dt);
+                    ?>
+                <a href="#" class="list-group-item @if($i == 0) active @endif">
                     <i class="flaticon-cloudy"></i>
-                    <div>Tue, 34°F</div>
+                    <div>{{iconv('latin5','utf-8',strftime(' %a ',strtotime($tarih)))}}, {{substr($gelen->main->temp, 0, 2)}}°C</div>
                 </a>
-                <a href="#" class="list-group-item">
-                    <i class="flaticon-sun"></i>
-                    <div>Wed, 38°F</div>
-                </a>
-                <a href="#" class="list-group-item">
-                    <i class="flaticon-cloud"></i>
-                    <div>thu, 32°F</div>
-                </a>
-                <a href="#" class="list-group-item">
-                    <i class="flaticon-rain"></i>
-                    <div>Fri, 31°F</div>
-                </a>
+                @endfor
             </div>
         </div>
         <div class="col-xs-9 col-sm-8 col-md-9 col-lg-9 bhoechie-tab thm-padding">
             <!-- weather temperature -->
             <div class="weather-temp-wrap active">
                 <div class="city-day">
-                    <div class="city">Dhaka</div>
-                    <div class="day">Tuesday, 8.00pm</div>
+                    <div class="city">Sakarya</div>
+                    <div class="day">
+                        <?php
+                        echo iconv('latin5','utf-8',strftime(' %d %B %Y %A %H:%M ',strtotime(date('Y-m-d H:i'))));
+                        ?>
+                    </div>
                 </div>
                 <div class="weather-icon">
                     <i class="flaticon-cloudy"></i>
-                    <div class="main-temp">34°F</div>
+                    <div class="main-temp">
+                        {{substr($coz->list[0]->main->temp, 0, 2)}}°C</div>
                 </div>
                 <div class="break">
-                    <div class="wind-condition"> Wind: 13mph WSW</div>
-                    <div class="humidity">Humidity: 91%</div>
+                    <div class="wind-condition"> Rüzgar: {{$coz->list[0]->wind->speed}}/Saat</div>
+                    <div class="humidity">Nem: {{$coz->list[0]->main->humidity}}%</div>
                 </div>
             </div>
             <!-- weather temperature -->
             <div class="weather-temp-wrap">
                 <div class="city-day">
-                    <div class="city">Dhaka</div>
-                    <div class="day">Wednesday, 8.00pm</div>
+                    <div class="city">Sakarya</div>
+                    <div class="day">
+                    </div>
                 </div>
                 <div class="weather-icon">
                     <i class="flaticon-sun"></i>
